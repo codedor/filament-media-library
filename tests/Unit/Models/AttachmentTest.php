1a0a3654debe688f191b8188d30fa79bb26d32c2
De Langhe Thibo <38117009+thibodelanghe@users.noreply.github.com>
2023-03-27 16:28:30 +0200

    Added tests for format url generation

--- tests/Unit/Models/AttachmentTest.php
+++ tests/Unit/Models/AttachmentTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use Codedor\Attachments\Facades\Models;
 use Codedor\Attachments\Models\Attachment;
+use Codedor\Attachments\Tests\TestModels\TestModel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
@@ -25,4 +27,42 @@ it('deletes root directory when attachment is removed from db', function () {
     $attachment->delete();
 
     Storage::assertMissing($attachment->directory);
+});
+
+it('returns the right url for a format', function () {
+    Models::add(TestModel::class);
+
+    /** @var Attachment $attachment */
+    $attachment = Attachment::factory([
+        'type' => 'image',
+        'extension' => 'jpg',
+        'disk' => 'public',
+    ])->create();
+
+    expect($attachment->getFormat('test-hero'))
+        ->toEndWith("test_hero__$attachment->filename");
+});
+
+it('returns null for a format that does not exist', function () {
+    /** @var Attachment $attachment */
+    $attachment = Attachment::factory([
+        'type' => 'image',
+        'extension' => 'jpg',
+        'disk' => 'public',
+    ])->create();
+
+    expect($attachment->getFormat('format-does-not-exist'))
+        ->toBeNull();
+});
+
+it('returns the original image when unknown format is requested', function () {
+    /** @var Attachment $attachment */
+    $attachment = Attachment::factory([
+        'type' => 'image',
+        'extension' => 'jpg',
+        'disk' => 'public',
+    ])->create();
+
+    expect($attachment->getFormatOrOriginal('test-hero'))
+        ->toEndWith("$attachment->filename");
 });
