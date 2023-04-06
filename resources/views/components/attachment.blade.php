f91b74ffc24c68b9700490747b94b287a7c41f30
De Langhe Thibo <38117009+thibodelanghe@users.noreply.github.com>
2023-04-06 14:10:57 +0200

    Update attachment.blade.php

--- resources/views/components/attachment.blade.php
+++ resources/views/components/attachment.blade.php
@@ -1,5 +1,5 @@
 <div
-    style="background-image: url('{{ $attachment->url() }}')"
+    style="background-image: url('{{ $attachment->url }}')"
     {{ $attributes->except(['slot', 'attachment'])->merge(['class' => '
         flex relative w-full aspect-square rounded-lg overflow-hidden
         bg-center bg-contain bg-no-repeat bg-gray-100 border
