1c46cd311e1fd8e39e89e6980d066b53410a707a
AngryMoustache <AngryMoustache@users.noreply.github.com>
2023-04-27 12:58:44 +0000

    Fix styling

--- database/migrations/2023_04_27_120359_create_attachment_formats.php
+++ database/migrations/2023_04_27_120359_create_attachment_formats.php
@@ -1,9 +1,6 @@
 <?php
 
-use Codedor\Attachments\Models\Attachment;
-use Codedor\Attachments\Models\AttachmentTag;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
