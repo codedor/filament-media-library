38fd731761a0e58aa5d741c5d905254695d81453
Sander Van Damme <sander.vandamme@onlyhumans.com>
2023-09-22 10:31:50 +0000

    WIP attachment picker

--- src/Filament/AttachmentInput.php
+++ src/Filament/AttachmentInput.php
@@ -128,6 +128,9 @@ class AttachmentInput extends Field
                         'isMultiple' => $component->isMultiple(),
                         'isGrid' => true,
                     ]);
+                })
+                ->action(function (array $arguments, Component $livewire) {
+                    data_set($livewire, $arguments['statePath'], $arguments['resources']);
                 }),
         ]);
     }
