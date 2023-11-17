b17a683644d3def69d984691ec4c5de9f548660c
AngryMoustache <AngryMoustache@users.noreply.github.com>
2023-11-17 15:10:28 +0000

    Fix styling

--- src/Models/Traits/HasFormats.php
+++ src/Models/Traits/HasFormats.php
@@ -23,7 +23,7 @@ trait HasFormats
         return $this->getFormat($name) ?? $this->url;
     }
 
-    public function getFormat(string $name, ?string $extension = null): ?string
+    public function getFormat(string $name, string $extension = null): ?string
     {
         if (! is_convertible_image($this->extension)) {
             return $this->url;
@@ -52,7 +52,7 @@ trait HasFormats
         Formats::dispatchGeneration($this, $force);
     }
 
-    public function getWebpFormatOrOriginal(string $format) : ?string
+    public function getWebpFormatOrOriginal(string $format): ?string
     {
         return $this->getFormat($format, 'webp') ?? $this->getFormatOrOriginal($format);
     }
