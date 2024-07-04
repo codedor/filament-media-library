b3bbe6dcb9b72ce863d730a228ba1213742e561d
Sander Van Damme <sander.vandamme@onlyhumans.com>
2024-07-04 11:46:52 +0000

    Allow user to set a specific queue for attachments

--- config/filament-media-library.php
+++ config/filament-media-library.php
@@ -6,6 +6,7 @@ return [
     'conversion' => LocalConversion::class,
     'enable-format-generate-action' => true,
     'enable-webp-generation' => true,
+    'format-queue' => 'media',
     'extensions' => [
         'image' => [
             'jpg',
