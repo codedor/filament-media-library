63198c05abac504da21a76394a729c0cf3616e2d
Thibaut Degezelle <thibaut.degezelle1@gmail.com>
2024-10-18 16:18:39 +0200

    THE001-90 add get_resource_url_by_model method

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,9 @@
 <?php
 
 use Codedor\MediaLibrary\Facades\Formats;
+use Filament\Facades\Filament;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Resources\Resource;
 
 if (! function_exists('get_placeholder_url_by_format')) {
     function get_placeholder_url_by_format($format)
@@ -26,5 +29,27 @@ if (! function_exists('is_image_with_dimensions')) {
     function is_image_with_dimensions(string $extension): bool
     {
         return in_array($extension, config('filament-media-library.extensions.image', [])) && $extension !== 'svg';
+    }
+}
+
+if (! function_exists('get_resource_url_by_model')) {
+    function get_resource_url_by_model(Model $model, $action = 'index')
+    {
+        $resourceClass = get_class($model);
+
+        $filamentResource = collect(Filament::getResources())
+            ->first(function (Resource $filamentResource) use ($resourceClass) {
+                return $filamentResource::getModel() === $resourceClass;
+            });
+
+        if (! $filamentResource) {
+            return null;
+        }
+
+        if ($action === 'index') {
+            return $filamentResource::getUrl();
+        }
+
+        return $filamentResource::getUrl($action, $model);
     }
 }
