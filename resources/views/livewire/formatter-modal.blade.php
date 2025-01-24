5d0a930d6121129dc9290a7af2f3840afc153d5d
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-01-24 15:36:41 +0200

    Add s3 support (#30)
    * Add s3 support
    
    * Set crossorigin to false, so cropper works with s3 urls

--- resources/views/livewire/formatter-modal.blade.php
+++ resources/views/livewire/formatter-modal.blade.php
@@ -41,6 +41,7 @@
                         ready() {
                             window.cropper.setData(previousData || {})
                         },
+                        checkCrossOrigin: false,
                     })
                 },
                 submit () {
@@ -79,6 +80,7 @@
                             id="filament-media-library::formatter"
                             wire:key="filament-media-library::formatter-{{ $attachment->id }}"
                             style="max-width: 100%; max-height: 68vh"
+                            crossorigin="anonymous"
                         >
                     </div>
 
---
+++ src/Conversions/S3Conversion.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Codedor\MediaLibrary\Conversions;
+
+use Codedor\MediaLibrary\Formats\Format;
+use Codedor\MediaLibrary\Models\Attachment;
+use Codedor\MediaLibrary\Support\TemporaryDirectory;
+use Illuminate\Support\Str;
+use Spatie\Image\Image;
+
+class S3Conversion implements Conversion
+{
+    public function convert(Attachment $attachment, Format $format, bool $force = false): bool
+    {
+        if (! is_convertible_image($attachment->extension)) {
+            return false;
+        }
+
+        $formatName = $format->filename($attachment);
+        $savePath = $attachment->directory . '/' . $formatName;
+
+        if (array_key_exists('format', $format->definition()->toArray()[0])) {
+            $savePath = Str::replaceLast(
+                $attachment->extension,
+                $format->definition()->toArray()[0]['format'],
+                $savePath
+            );
+        }
+
+        $formatPath = "$attachment->directory/$formatName";
+
+        if (
+            $force ||
+            ! $attachment->getStorage()->exists($formatPath)
+        ) {
+            $temporaryDirectory = TemporaryDirectory::create();
+            $tempPath = $temporaryDirectory->path(Str::random(16) . '.' . $attachment->extension);
+            $disk = $attachment->getStorage();
+
+            file_put_contents($tempPath, $disk->readStream($attachment->file_path));
+
+            Image::load($tempPath)
+                ->manipulate($format->definition())
+                ->save();
+
+            $file = fopen($tempPath, 'r');
+
+            $disk->put(
+                $formatPath,
+                $file,
+            );
+
+            if (is_resource($file)) {
+                fclose($file);
+            }
+
+            $temporaryDirectory->delete();
+        }
+
+        return true;
+    }
+}
