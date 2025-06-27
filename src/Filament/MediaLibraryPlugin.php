5ef949d0121aabbfc82718f814bce5498e0bd477
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-06-27 15:45:46 +0300

    Do not import tailwind in plugin.css, since we will include it in our custom theme in project

--- src/Filament/MediaLibraryPlugin.php
+++ src/Filament/MediaLibraryPlugin.php
@@ -47,7 +47,6 @@ class MediaLibraryPlugin implements Plugin
         );
 
         FilamentAsset::register([
-            Css::make('filament-media-library-stylesheet', __DIR__ . '/../../dist/css/laravel-media.css'),
             Css::make('filament-media-library-cropper-stylesheet', __DIR__ . '/../../dist/css/cropper.min.css'),
             Js::make('filament-media-library-script', __DIR__ . '/../../dist/js/cropper.min.js'),
         ], 'codedor/filament-media-library');
